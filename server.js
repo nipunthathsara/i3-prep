f74ae8ec7b60b45f0dc6fd77328441334240a1d3
nipunthathsara <t-nipunt@brandix.com>
2016-08-09 13:55:03 +0530

    model added

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-var database = require('./config/database')
+var express = require('express');
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var _ = require('lodash');
+
+var database = require('./config/database');
